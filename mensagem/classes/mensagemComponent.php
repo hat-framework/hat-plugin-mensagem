5dc90166e8391a4d41ecb0ee7be6f8f6c30411b1
tigredonorte <tigredonorte3@gmail.com>
2014-10-20 08:18:48 -0200

    B3

--- mensagem/classes/mensagemComponent.php
+++ mensagem/classes/mensagemComponent.php
@@ -2,7 +2,7 @@
 class mensagemComponent extends classes\Component\Component{
     public function formulario(){
         $this->gui = new \classes\Component\GUI();
-        $class = 'span6';
+        $class = 'col-xs-6';
         $this->gui->widgetOpen('', $class);
             $this->gui->title('Envie mensagem para todos os usuários do site através dessa plataforma. '
                     . 'Caso queira enviar para todos os perfis, selecione o perfil Administrador.');
