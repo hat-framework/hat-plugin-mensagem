ec77671b7bcdcf679924c0f29d2c5366292790b6
tigredonorte <tigredonorte3@gmail.com>
2014-12-08 10:00:35 -0200

    dcoracoes restrict user chat access

--- Config/mensagemLoader.php
+++ Config/mensagemLoader.php
@@ -2,7 +2,12 @@
 
 class mensagemLoader extends classes\Classes\PluginLoader{
 
-    public function setCommonVars(){}
+    public function setCommonVars(){
+        if(false === strstr($_SERVER["SERVER_NAME"], 'dcoracoes')){return;}
+        if(!in_array(usuario_loginModel::CodPerfil(), array(Visitante,'20', '15'))){
+            die("Você não tem permissão para acessar esta página!");
+        }
+    }
     
     public function setAdminVars(){}
     
