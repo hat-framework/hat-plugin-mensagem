0eccc323ce99d52935a805fe6a9e9e9b78f76e48
tigredonorte <tigredonorte3@gmail.com>
2015-03-05 13:54:28 -0300

    bugfixed in dcoracoes case

--- Config/mensagemLoader.php
+++ Config/mensagemLoader.php
@@ -4,8 +4,12 @@ class mensagemLoader extends classes\Classes\PluginLoader{
 
     public function setCommonVars(){
         if(false === strstr($_SERVER["SERVER_NAME"], 'dcoracoes')){return;}
-        if(!in_array(usuario_loginModel::CodPerfil(), array(Visitante,'20', '15','3'))){
-            throw new classes\Exceptions\AcessDeniedException("Você não tem permissão para acessar esta página!");
+        $cod_perfil = usuario_loginModel::CodPerfil();
+        if(!in_array($cod_perfil, array('3','2',Visitante,'4','20', '15'))){
+            throw new classes\Exceptions\AcessDeniedException(
+                     "O seu perfil de usuário ($cod_perfil) não está credenciado a acessar esta página. "
+                    ."Podem acessar a página apenas <br>: Administradores, Visitantes, Moderadores de Mensagem, Assinantes Temporários e Revendedoras"
+            );
         }
     }
     
